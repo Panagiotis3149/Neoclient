fddae02b84d6c30e3138087e4694cda64dc6ff29
Panagiotis3149 <138581609+Panagiotis3149@users.noreply.github.com>
2025-02-19 17:17:58 +0200

    + Make buildable (i hope so, again)

--- src/main/java/keystrokesmod/Variables.java
+++ src/main/java/keystrokesmod/Variables.java
@@ -1,6 +1,6 @@
 package keystrokesmod;
 
 public class Variables {
-    public static String clientVersion = "v0.9.9 Dev";
+    public static String clientVersion = "v0.9.9 Stable";
     public static String clientName = "Neoclient";
 }
