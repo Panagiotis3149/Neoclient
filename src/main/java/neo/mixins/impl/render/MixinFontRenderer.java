da4fa2ca06bf30cc57e845d34756d9793805bf4d
Panagiotis3149 <138581609+Panagiotis3149@users.noreply.github.com>
2025-08-10 10:35:57 +0300

    + newline neotheme fix

--- src/main/java/neo/mixins/impl/render/MixinFontRenderer.java
+++ src/main/java/neo/mixins/impl/render/MixinFontRenderer.java
@@ -4,8 +4,6 @@ import neo.module.ModuleManager;
 import neo.module.impl.client.ClientTheme;
 import neo.module.impl.client.Settings;
 import neo.module.impl.other.NameHider;
-
-import neo.util.Utils;
 import neo.util.render.Theme;
 import net.minecraft.client.gui.FontRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,11 +11,6 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import java.awt.*;
-import java.util.Locale;
-
-import static neo.util.render.RenderUtils.interpolateColorC;
 
 @Mixin(FontRenderer.class)
 public class MixinFontRenderer {
@@ -51,6 +44,9 @@ public class MixinFontRenderer {
     @Shadow
     private float func_181559_a(char c, boolean italic) { return 0f; }
 
+    @Shadow private int FONT_HEIGHT;
+
+
     @Shadow
     private void doDraw(float f) { }
 
@@ -66,37 +62,102 @@ public class MixinFontRenderer {
         return string;
     }
 
+    /**
+     * @author Mojang?? i (Panagiotis3149) just added text theming
+     * @reason Text Theming
+     */
     @Overwrite
     private void renderStringAtPos(String text, boolean shadow) {
         int themeIndex = (int) ClientTheme.theme.getInput();
         double timeMult = Settings.timeMultiplier.getInput();
+        float lineHeight = 9f;
 
         for (int i = 0; i < text.length(); i++) {
             if (text.startsWith("$NEOTHEME$", i)) {
                 int end = text.indexOf("$NEOTHEME$", i + 10);
-                if (end == -1) break;
-
+                if (end == -1) {
+                    // treat as normal text (fall through)
+                } else {
                     String themed = text.substring(i + 10, end);
-                    for (int j = 0; j < themed.length(); j++) {
-                        char c = themed.charAt(j);
-                        double delay = j * timeMult;
+                    String[] parts = themed.split("\n", -1);
+                    for (int p = 0; p < parts.length; p++) {
+                        String part = parts[p];
+                        for (int j = 0; j < part.length(); j++) {
+                            char c = part.charAt(j);
+                            double delay = j * timeMult;
 
-                        if (!shadow) {
-                            int rgb = Theme.getGradient(themeIndex, delay);
-                            float r = (rgb >> 16 & 0xFF) / 255f;
-                            float g = (rgb >> 8 & 0xFF) / 255f;
-                            float b = (rgb & 0xFF) / 255f;
-                            setColor(r, g, b, this.alpha);
+                            if (c == 167 && j + 1 < part.length()) {
+                                int i1 = "0123456789abcdefklmnor".indexOf(Character.toLowerCase(part.charAt(j + 1)));
+
+                                if (i1 >= 0 && i1 <= 15) {
+                                    this.randomStyle = false;
+                                    this.boldStyle = false;
+                                    this.strikethroughStyle = false;
+                                    this.underlineStyle = false;
+                                    this.italicStyle = false;
+
+                                    int color = this.colorCode[shadow ? i1 + 16 : i1];
+                                    this.textColor = color;
+                                    this.red = (color >> 16) & 0xFF;
+                                    this.blue = (color >> 8) & 0xFF;
+                                    this.green = color & 0xFF;
+                                    if (!shadow) {
+                                        int rgb = Theme.getGradient(themeIndex, delay);
+                                        float r = (rgb >> 16 & 0xFF) / 255f;
+                                        float g = (rgb >> 8 & 0xFF) / 255f;
+                                        float b = (rgb & 0xFF) / 255f;
+                                        setColor(r, g, b, this.alpha);
+                                    } else {
+                                        setColor((color >> 16) / 255f, (color >> 8 & 255) / 255f, (color & 255) / 255f, this.alpha);
+                                    }
+                                } else if (i1 == 16) this.randomStyle = true;
+                                else if (i1 == 17) this.boldStyle = true;
+                                else if (i1 == 18) this.strikethroughStyle = true;
+                                else if (i1 == 19) this.underlineStyle = true;
+                                else if (i1 == 20) this.italicStyle = true;
+                                else if (i1 == 21) {
+                                    this.randomStyle = false;
+                                    this.boldStyle = false;
+                                    this.strikethroughStyle = false;
+                                    this.underlineStyle = false;
+                                    this.italicStyle = false;
+                                    setColor(this.red, this.blue, this.green, this.alpha);
+                                }
+
+                                j++;
+                                continue;
+                            }
+
+                            if (!shadow) {
+                                int rgb = Theme.getGradient(themeIndex, delay);
+                                float r = (rgb >> 16 & 0xFF) / 255f;
+                                float g = (rgb >> 8 & 0xFF) / 255f;
+                                float b = (rgb & 0xFF) / 255f;
+                                setColor(r, g, b, this.alpha);
+                            }
+
+                            float f = this.func_181559_a(c, this.italicStyle);
+                            if (this.boldStyle) {
+                                this.posX += (this.unicodeFlag ? 0.5f : 1f);
+                                this.func_181559_a(c, this.italicStyle);
+                                this.posX -= (this.unicodeFlag ? 0.5f : 1f);
+                                f++;
+                            }
+
+                            doDraw(f);
                         }
 
-                        float f = this.func_181559_a(c, this.italicStyle);
-                        doDraw(f);
+                        if (p < parts.length - 1) {
+                            this.posX = 0f;
+                            this.posY += lineHeight;
+                        }
+                    }
 
+                    i = end + "$NEOTHEME$".length() - 1;
+                    continue;
                 }
-
-                i = end + "$NEOTHEME$".length() - 1;
-                continue;
             }
+
             char c0 = text.charAt(i);
             if (c0 == 167 && i + 1 < text.length()) {
                 int i1 = "0123456789abcdefklmnor".indexOf(Character.toLowerCase(text.charAt(i + 1)));
@@ -125,11 +186,10 @@ public class MixinFontRenderer {
                     setColor(this.red, this.blue, this.green, this.alpha);
                 }
 
-                i++; // skip next char
+                i++;
                 continue;
             }
 
-            // --- Normal character rendering ---
             float f = this.func_181559_a(c0, this.italicStyle);
             if (this.boldStyle) {
                 this.posX += (this.unicodeFlag ? 0.5f : 1f);
@@ -145,17 +205,17 @@ public class MixinFontRenderer {
 
 
 
+
+
     @ModifyVariable(method = "getStringWidth", at = @At("HEAD"), require = 1, ordinal = 0, argsOnly = true)
     private String getStringWidth(String string) {
-
         if (string == null) return null;
-        string = string.replaceAll("\\$NEOTHEME\\$.*?\\$NEOTHEME\\$", "");
-
+        string = string.replaceAll("(?s)\\$NEOTHEME\\$(.*?)\\$NEOTHEME\\$", "$1");
         if ((ModuleManager.nameHider != null) && ModuleManager.nameHider.isEnabled()) {
             string = NameHider.getFakeName(string);
-            string = string.replaceAll("\\$NEOTHEME\\$.*?\\$NEOTHEME\\$", "");
         }
-
+        string = string.replace("$NEOTHEME$", "");
         return string;
     }
+
 }
